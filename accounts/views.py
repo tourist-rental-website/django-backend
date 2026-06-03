694bec8fe3e778e266cc0cd97539cf987f4088dd
aayush <shresthaaayushh@gmail.com>
2026-06-03 23:30:58 +0545

    feat: add template and css for register

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
 def login_view(request):
+    
     return render(request, 'accounts/login.html')
 
 def register_view(request):
-    return render(request, 'accounts/register.html')
+    context = {
+        'title': ("Register"),
+        'page_title': ("Register Account"),
+        'page_description': ('this is the registration page'),
+    }
+    return render(request, 'accounts/register.html', context)
 
 def logout_view(request):
     return render(request, 'accounts/login.html')
