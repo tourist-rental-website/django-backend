826dda66e7281a88c5ea05282cfe93f0ff394144
aayush <shresthaaayushh@gmail.com>
2026-06-03 22:48:38 +0545

    feat:basic login and register endpoint for accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login_view(request):
+    return render(request, 'accounts/login.html')
+
+def register_view(request):
+    return render(request, 'accounts/register.html')
+
+def logout_view(request):
+    return render(request, 'accounts/login.html')
