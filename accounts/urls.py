fb55f9cb677d68b0561f5bf28de2a11aa2b3e7c3
gaurav pant <gauravpant870@gmail.com>
2026-06-06 17:15:08 +0545

    jwt-login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import RegisterView
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view()), # This view will handle user login and return JWT(acess and refresh) tokens
+    path("api/token/refresh/", TokenRefreshView.as_view()), # This view will handle refreshing JWT access tokens
 ]
+
