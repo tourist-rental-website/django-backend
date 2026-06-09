e57dabbcbbd98fec309757b53b3496c342b71e36
gaurav pant <gauravpant870@gmail.com>
2026-06-09 12:45:03 +0545

    HotelProfile

--- listings/models.py
+++ listings/models.py
@@ -3,8 +3,8 @@ from accounts.models import User
 
 
 class GuideProfile(models.Model):
-    user = models.OneToOneField(
-        User,
+    user = models.OneToOneField( # One-to-one relationship with User model, just like foregin key but only one guide profile per user 
+        User,                    # whereas foreign key allows multiple guide profiles for a single user
         on_delete=models.CASCADE,
         related_name="guide_profile"
     )
@@ -21,4 +21,19 @@ class GuideProfile(models.Model):
         return f"Guide: {self.user.email}"
     
 
-    
+class HotelProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="hotel_profile"
+    )
+
+    hotel_name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=255)
+    contact_number = models.CharField(max_length=20)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.hotel_name
--- listings/urls.py
+++ listings/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import GuideProfileCreateView, GuideProfileListView
+from .views import GuideProfileCreateView, GuideProfileListView, HotelProfileCreateView, HotelProfileListView
 
 urlpatterns = [
     path("guides/create/", GuideProfileCreateView.as_view(), name="guide-create"),
     path("guides/", GuideProfileListView.as_view(), name="guide-list"),
+    path("hotels/create/", HotelProfileCreateView.as_view(), name="hotel-create"),
+    path("hotels/", HotelProfileListView.as_view(), name="hotel-list"),
 ]
