81e42b45af81003648d1da49c2d0fa73847a6bba
aayush <shresthaaayushh@gmail.com>
2026-06-10 08:30:38 +0545

    feat: add rooms and package view urls and admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,4 +1,9 @@
+from importlib.resources import Package
+
 from django.contrib import admin
-from .models import GuideProfile
+from .models import GuideProfile, HotelProfile, Room
 # Register your models here.
 admin.site.register(GuideProfile)
+admin.site.register(HotelProfile)
+admin.site.register(Room)
+admin.site.register(Package)
