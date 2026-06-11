cd88d9b55641eb3277aa69907a16e0d64a4662fd
gaurav pant <gauravpant870@gmail.com>
2026-06-11 17:30:02 +0545

    bookings

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from jsonschema import ValidationError
+from .models import RoomBooking, PackageBooking
+from .serializers import RoomBookingSerializer, PackageBookingSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+class RoomBookingCreateView(generics.CreateAPIView):
+    serializer_class = RoomBookingSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        if self.request.user.role != 'traveler': # Check if the authenticated user has the role of 'traveler'
+            raise ValidationError("Only traveler users can create room bookings.")
+        serializer.save(traveler=self.request.user)
+
+class PackageBookingCreateView(generics.CreateAPIView):
+    serializer_class = PackageBookingSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(traveler=self.request.user)
